4d0070310c2425a0b18f72082e44c87590a7416e
A-Elbana <manelbana094@gmail.com>
2025-12-05 15:23:39 +0200

    Set up server workspace

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,39 +1,14 @@
 import express from "express";
-import { PrismaClient } from "@prisma/client";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config(); // loads .env
 
 const app = express();
-const prisma = new PrismaClient();
+app.use(cors());
 app.use(express.json());
 
-app.get("/users", async (_, res) => {
-  const users = await prisma.user.findMany({
-    where: {
-      nationality: {
-        in: ["Irish", "German", "Portuguese"],
-      },
-    },
-  });
-  res.json(users);
+app.get("/", (req, res) => {
+  res.json({ message: "Server running 👌" });
 });
 
-app.put("/users", async (_, res) => {
-  const updatedUser = await prisma.user.update({
-    where: { email: "pedro@example.com" },
-    data: {
-      age: 35,
-      isMarried: true,
-    },
-  });
-  res.json(updatedUser);
-});
-
-app.delete("/users", async (_, res) => {
-  const deletedUsers = await prisma.user.deleteMany({
-    where: { age: { gt: 30 } },
-  });
-  res.json(deletedUsers);
-});
-
-app.listen(4000, () => {
-  console.log("Server running on port 4000");
-});
