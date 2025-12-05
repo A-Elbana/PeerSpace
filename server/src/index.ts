077390ad460a48a2b31b725e4ef3171b9f470ab9
A-Elbana <manelbana094@gmail.com>
2025-12-05 15:48:01 +0200

    feat: Initialize server with Express, Prisma, and core dependencies.

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,3 +12,9 @@ app.get("/", (req, res) => {
   res.json({ message: "Server running 👌" });
 });
 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});
+
